cab266f4bcbcabe187920be6f2d2a825e746d3eb
siciliagc <s.garciac.2020@alumnos.urjc.es>
2023-11-21 21:24:56 +0100

    Avance :smiley:

--- TFG_Sicilia/pruebasColumnasFallo.py
+++ TFG_Sicilia/pruebasColumnasFallo.py
@@ -24,7 +24,7 @@ print("He leído ya el dataset. Procedo a eliminar y tratar los datos.")
 # Cogemos todas las columnas menos la etiqueta:
 
 data_X = df.drop('Label', axis=1)  # All columns except "label"
-data_X = pd.get_dummies(data_X)
+data_X = df.drop('Flow ID', axis=1)  # Esto es porque esta columna es la recopilación de otros datos que aparecen después
 data_y = df['Label']
 
 # Dividimos los datos del DF:
@@ -38,51 +38,116 @@ print("Cambiando 'ddos' a 1 y cualquier otro caso a 0")
 data_Y_train = data_Y_train.apply(lambda x: 1 if x == 'ddos' else 0)
 data_y_test = data_y_test.apply(lambda x: 1 if x == 'ddos' else 0)
 
+"""for index, value in data_X_train['Src IP'].items():
+    data_X_train.at[index, 'Src IP'] = value.replace('.', '')
+print(data_X['Src IP'])"""
 
-categorical_columns = data_X.select_dtypes(include=['object']).columns
-informe = []
+for index, value in data_X_train['Src IP'].items():
+    data_X_train.at[index, 'Src IP'] = value.replace('.', '')
 
+for index, value in data_X_train['Dst IP'].items():
+    data_X_train.at[index, 'Dst IP'] = value.replace('.', '')
 
-# Bucle para iterar sobre las columnas
-for i in range(1, len(data_X.columns) + 1):
-    # Generar todas las combinaciones posibles de columnas
-    column_combinations = [data_X.columns[j:j+i] for j in range(len(data_X.columns) - i + 1)]
+for index, value in data_X_test['Src IP'].items():
+    data_X_test.at[index, 'Src IP'] = value.replace('.', '')
 
-    # Iterar sobre las combinaciones de columnas
-    for columns_subset in column_combinations:
-        # Seleccionar el subconjunto de columnas
-        subconjunto_X_train = data_X_train[columns_subset]
-        subconjunto_X_test = data_X_test[columns_subset]
-
-        # Aplicar la transformación one-hot solo a las columnas categóricas
-        preprocessor = ColumnTransformer(
-            transformers=[
-                ('cat', OneHotEncoder(), categorical_columns)
-            ],
-            remainder='passthrough'
-        )
-        subconjunto_X_train_encoded = preprocessor.fit_transform(subconjunto_X_train)
-        subconjunto_X_test_encoded = preprocessor.transform(subconjunto_X_test)
-
-        # Entrenar el modelo
-        k = 5
-        clf = KNeighborsClassifier(n_neighbors=k)
-        clf.fit(subconjunto_X_train_encoded, data_Y_train)
-
-        # Hacer las predicciones
-        data_y_pred = clf.predict(subconjunto_X_test_encoded)
-
-        # Calcular la precisión
-        accuracy = accuracy_score(data_y_test, data_y_pred)
-
-        # Almacenar resultados en el informe
-        informe.append({
-            'Num_Columnas': i,
-            'Columnas': columns_subset,
-            'Precisión': accuracy
-        })
+for index, value in data_X_test['Dst IP'].items():
+    data_X_test.at[index, 'Dst IP'] = value.replace('.', '')
 
 
-for resultado in informe:
-    print(f"\nUtilizando las columnas: {resultado['Columnas']}")
-    print(f"Precisión: {resultado['Precisión']}")
+# Eliminar NaN de las primeras 5 columnas
+
+# Columna 1:
+
+for index, value in data_X_train['num'].items():
+    if pd.isna(data_X_train.at[index, 'num']):
+        data_X_train.at[index, 'num'] = 0
+
+for index, value in data_X_test['num'].items():
+    if pd.isna(data_X_test.at[index, 'num']):
+        data_X_test.at[index, 'num'] = 0
+
+# Columna 2:
+
+for index, value in data_X_train['Src IP'].items():
+    if pd.isna(data_X_train.at[index, 'Src IP']):
+        data_X_train.at[index, 'Src IP'] = 0
+
+for index, value in data_X_test['Src IP'].items():
+    if pd.isna(data_X_test.at[index, 'Src IP']):
+        data_X_test.at[index, 'Src IP'] = 0
+
+# Columna 3:
+
+for index, value in data_X_train['Src Port'].items():
+    if pd.isna(data_X_train.at[index, 'Src Port']):
+        data_X_train.at[index, 'Src Port'] = 0
+
+for index, value in data_X_test['Src Port'].items():
+    if pd.isna(data_X_test.at[index, 'Src Port']):
+        data_X_test.at[index, 'Src Port'] = 0
+
+# Columna 4:
+
+for index, value in data_X_train['Dst Port'].items():
+    if pd.isna(data_X_train.at[index, 'Dst Port']):
+        data_X_train.at[index, 'Dst Port'] = 0
+
+for index, value in data_X_test['Dst Port'].items():
+    if pd.isna(data_X_test.at[index, 'Dst Port']):
+        data_X_train.at[index, 'Dst Port'] = 0
+
+# Columna 5:
+
+for index, value in data_X_train['Dst IP'].items():
+    if pd.isna(data_X_train.at[index, 'Dst IP']):
+        data_X_train.at[index, 'Dst IP'] = 0
+
+for index, value in data_X_test['Dst IP'].items():
+    if pd.isna(data_X_test.at[index, 'Dst IP']):
+        data_X_train.at[index, 'Dst IP'] = 0
+
+
+# Columna 6:
+
+for index, value in data_X_train['Protocol'].items():
+    if pd.isna(data_X_train.at[index, 'Protocol']):
+        data_X_train.at[index, 'Protocol'] = 0
+
+for index, value in data_X_test['Protocol'].items():
+    if pd.isna(data_X_test.at[index, 'Protocol']):
+        data_X_train.at[index, 'Protocol'] = 0
+
+"""
+
+print(data_X_train)
+
+subset_X_train = data_X_train.iloc[:, :5]
+print("Hola")
+print(subset_X_train)"""
+
+# Entrenar el modelo
+k = 5
+clf = KNeighborsClassifier(n_neighbors=k)
+
+
+subset_X_train = data_X_train.iloc[:, :6]
+subset_X_test = data_X_test.iloc[:, :6]
+
+"""print(subset_X_train)
+
+print("********************************************")
+print("********************************************")
+print("********************************************")
+print("********************************************")
+
+print(subset_X_test)"""
+
+
+clf.fit(subset_X_train, data_Y_train)
+
+print("Vamos a predecir")
+data_y_pred = clf.predict(subset_X_test)
+
+accuracy = accuracy_score(data_y_test, data_y_pred)
+print(accuracy)
