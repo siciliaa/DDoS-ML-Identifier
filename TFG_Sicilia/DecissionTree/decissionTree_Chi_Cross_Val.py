8a4d8ab606fc543457bca0ecd57504d26be17be4
siciliagc <s.garciac.2020@alumnos.urjc.es>
2024-06-11 20:51:04 +0200

    Updates

--- TFG_Sicilia/DecissionTree/decissionTree_Chi_Cross_Val.py
+++ TFG_Sicilia/DecissionTree/decissionTree_Chi_Cross_Val.py
@@ -1,40 +1,34 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import export_graphviz
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.feature_selection import chi2, SelectKBest
-from subprocess import call
-from datetime import datetime
-import time
 import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from datetime import datetime
+from sklearn import tree
+from sklearn.feature_selection import chi2, SelectKBest
+import graphviz
 import ipaddress
+import time
 import os
 
 # Leemos el DF
-path = r'../final_dataset.csv/final_dataset.csv'
 start_time = time.time()
+path = r'../final_dataset.csv/final_dataset.csv'
 df = pd.read_csv(path)
 print("He leído ya el dataset.")
 leido = time.time()
+
 tiempo_lectura_dataset = leido - start_time
 
 # Cogemos todas las columnas menos la etiqueta y flowID
-columnas_eliminar = ['Label', 'Flow ID', 'Fwd Seg Size Avg', 'Subflow Fwd Byts', 'Bwd Pkt Len Mean', 'Tot Fwd Pkts',
-                     'Tot Bwd Pkts',
-                     'TotLen Bwd Pkts', 'Bwd Header Len', 'TotLen Fwd Pkts', 'Idle Mean', 'Subflow Fwd Pkts',
-                     'Pkt Len Mean',
+columnas_eliminar = ['Label', 'Flow ID', 'Fwd Seg Size Avg', 'Subflow Fwd Byts', 'Bwd Pkt Len Mean', 'Tot Fwd Pkts', 'Tot Bwd Pkts',
+                     'TotLen Bwd Pkts', 'Bwd Header Len', 'TotLen Fwd Pkts', 'Idle Mean', 'Subflow Fwd Pkts', 'Pkt Len Mean',
                      'Subflow Bwd Pkts', 'Flow IAT Mean', 'Idle Max', 'Flow Duration', 'Flow IAT Min', 'Pkt Len Max',
-                     'Fwd Pkt Len Max', 'Fwd IAT Mean', 'Bwd Pkt Len Max', 'Fwd Pkt Len Mean', 'Fwd Header Len',
-                     'Active Mean',
+                     'Fwd Pkt Len Max', 'Fwd IAT Mean', 'Bwd Pkt Len Max', 'Fwd Pkt Len Mean', 'Fwd Header Len', 'Active Mean',
                      'Flow IAT Max', 'Pkt Size Avg', 'Protocol', 'Bwd IAT Std', 'Bwd Pkt Len Std', 'Fwd Pkt Len Min',
                      'Fwd IAT Max', 'Flow IAT Std', 'Bwd IAT Mean', 'Fwd IAT Tot', 'Bwd Pkt Len Min', 'Active Std',
                      'SYN Flag Cnt', 'Bwd IAT Tot', 'Fwd Pkt Len Std', 'Pkt Len Std', 'Fwd IAT Std', 'Active Max',
-                     'RST Flag Cnt', 'Bwd IAT Max', 'Src Port', 'PSH Flag Cnt', 'CWE Flag Count', 'Fwd IAT Min',
-                     'Flow Byts/s',
-                     'ACK Flag Cnt', 'Dst Port', 'Fwd Byts/b Avg', 'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg',
-                     'Bwd Byts/b Avg',
+                     'RST Flag Cnt', 'Bwd IAT Max', 'Src Port', 'PSH Flag Cnt', 'CWE Flag Count', 'Fwd IAT Min', 'Flow Byts/s',
+                     'ACK Flag Cnt', 'Dst Port', 'Fwd Byts/b Avg', 'Fwd Pkts/b Avg', 'Fwd Blk Rate Avg', 'Bwd Byts/b Avg',
                      'Bwd Pkts/b Avg', 'Fwd URG Flags', 'Bwd URG Flags', 'Bwd Blk Rate Avg']
 
 data_X = df.drop(columnas_eliminar, axis=1)  # Todas las columnas excepto "label"
@@ -66,31 +60,49 @@ for index, value in data_X['Timestamp'].items():
     timestamp = fecha_hora_obj.timestamp()
     data_X.at[index, 'Timestamp'] = timestamp
 
+# Asegurarse de que todos los valores sean no negativos
 print("Asegurando que todos los valores sean no negativos")
 data_X = data_X.apply(lambda x: x - x.min() if x.min() < 0 else x)
 
 # Selección de características usando Chi-Square
-print("Seleccionando características más importantes usando Chi-Square")
-selector = SelectKBest(score_func=chi2, k=20)
+print("Seleccionando características con Chi-Square")
+selector = SelectKBest(score_func=chi2, k=25)
+
 data_X_selected = selector.fit_transform(data_X, data_y)
+
+# Obtener los nombres de las características seleccionadas
 selected_features = data_X.columns[selector.get_support()]
+
 print(f"Características seleccionadas: {selected_features}")
 
 # Dividimos los datos del DF
 print("Voy a dividir los datos del dataset")
-data_X_train, data_X_test, data_y_train, data_y_test = train_test_split(data_X_selected, data_y, test_size=0.2,
-                                                                        random_state=42)
+data_X_train, data_X_test, data_y_train, data_y_test = train_test_split(data_X_selected, data_y, test_size=0.2, random_state=42)
+
+# Entrenar el modelo con cross-validation
+print("Entrenando el modelo con validación cruzada")
+kf = KFold(n_splits=10, shuffle=True, random_state=68)
+clf = tree.DecisionTreeClassifier(random_state=42, max_depth=25)
+
+# Validación cruzada
+cv_scores = cross_val_score(clf, data_X_train, data_y_train, cv=kf, scoring='accuracy')
+# cv_scores = [round(num, 3) for num in cv_scores]
+print('Se obtienen los siguientes coeficientes de determinación:')
+print(cv_scores, '\n')
+print(f'Max Accuracy: {max(cv_scores)}')
+print(f'Min Accuracy: {min(cv_scores)}')
+print('Promedio Accuracy: {:.3f}'.format(np.mean(cv_scores)))
+print('Desviación Estándar: {:.3f}'.format(np.std(cv_scores)))
+print(f'Intervalo de confianza 95%: {np.quantile(cv_scores, [0.025, 0.975])}')
 
 # Entrenar el modelo
-clf = RandomForestClassifier(max_depth=25, random_state=42, n_estimators=20)
-
-print("Entrenar modelo")
+print("Entrenando el modelo")
 entrenar = time.time()
 clf = clf.fit(data_X_train, data_y_train)
 
-print("Vamos a predecir")
-# Predict
+# Predecir
 predecir = time.time()
+print("Vamos a predecir")
 prediction = clf.predict(data_X_test)
 fin_predecir = time.time()
 tiempo_en_predecir = fin_predecir - predecir
@@ -103,8 +115,7 @@ f1 = f1_score(data_y_test, prediction)
 total_time = fin_predecir - start_time
 
 print("********************************")
-print(
-    f"[*]Tiempo total de ejecución: {total_time}\n[*]Tiempo tardado en leer el dataset: {tiempo_lectura_dataset}\n[*]Tiempo en entrenar y predecir: {tiempo_en_entrenar_y_predecir}\n[*]Tiempo en predecir: {tiempo_en_predecir}")
+print(f"[*]Tiempo total de ejecución: {total_time}\n[*]Tiempo tardado en leer el dataset: {tiempo_lectura_dataset}\n[*]Tiempo en entrenar y predecir: {tiempo_en_entrenar_y_predecir}\n[*]Tiempo en predecir: {tiempo_en_predecir}")
 print("********************************")
 
 print("[*]Accuracy:", accuracy)
@@ -112,23 +123,13 @@ print("[*]Precision:", precision)
 print("[*]Recall (Sensibilidad):", recall)
 print("[*]F1 Score:", f1)
 
-# Crear directorio para almacenar los árboles si no existe
-output_dir = 'trees_25'
+# Crear el árbol y guardarlo
+print("Creando el árbol")
+dot_data = tree.export_graphviz(clf, out_file=None, feature_names=selected_features, filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data)
+
+# Guardar el árbol en formato PDF
+output_dir = 'trees_25_Cross'
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-
-# Exportar y guardar todos los árboles en archivos PDF
-feature_names = selected_features  # Cambia esto para que coincida con las características seleccionadas
-for i, estimator in enumerate(clf.estimators_):
-    dot_file = f'{output_dir}/tree_{i}.dot'
-    pdf_file = f'{output_dir}/tree_{i}.pdf'
-
-    # Exportar el árbol a un archivo .dot
-    export_graphviz(estimator, out_file=dot_file,
-                    feature_names=feature_names,
-                    rounded=True, proportion=False,
-                    precision=2, filled=True)
-
-    # Convertir el archivo .dot a .pdf
-    call(['dot', '-Tpdf', dot_file, '-o', pdf_file])
-    print(f'Árbol {i} guardado como {pdf_file}')
+graph.render(os.path.join(output_dir, 'decision_tree_25_Cross'), format='pdf', view=True)
--- TFG_Sicilia/pruebaValoresNulos.py
+++ TFG_Sicilia/pruebaValoresNulos.py
@@ -1,68 +1,15 @@
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-from datetime import datetime
 
-# Leemos el DF:
+# Leer el dataset desde un archivo CSV (puedes cambiar esto a tu fuente de datos)
+df = pd.read_csv('final_dataset.csv/final_dataset.csv', usecols=[0])
 
-path = r'final_dataset.csv/final_dataset.csv'
-df = pd.read_csv(path)
-print("He leído ya el dataset.")
+# Mostrar las primeras filas del DataFrame para confirmar que se ha leído correctamente
+print(df.head())
 
+# Obtener el nombre de la primera columna
+primer_columna_nombre = df.columns[0]
 
-# Cogemos todas las columnas menos la etiqueta, num y flowID:
-columnas_eliminar = ['Label', 'Flow ID']
+# Contar los elementos diferentes en la primera columna
+num_elementos_diferentes = df[primer_columna_nombre].nunique()
 
-data_X = df.drop(columnas_eliminar, axis=1)  # All columns except "label"
-data_y = df['Label']
-
-print("Vamos a quitarle el punto a las IPs")
-
-for index, value in data_X['Src IP'].items():
-    data_X.at[index, 'Src IP'] = value.replace('.', '')
-
-for index, value in data_X['Dst IP'].items():
-    data_X.at[index, 'Dst IP'] = value.replace('.', '')
-
-for index, value in data_X['Timestamp'].items():
-    if "AM" in value or "PM" in value:
-        fecha_hora_obj = datetime.strptime(value, "%d/%m/%Y %I:%M:%S %p")
-        timestamp = fecha_hora_obj.timestamp()
-        data_X.at[index, 'Timestamp'] = timestamp
-    else:
-        fecha_hora_obj = datetime.strptime(value, "%d/%m/%Y %H:%M:%S")
-        timestamp = fecha_hora_obj.timestamp()
-        data_X.at[index, 'Timestamp'] = timestamp
-
-print("Ahora vamos a cambiar 'ddos' a 1 y cualquier otro caso a 0 ")
-data_y = data_y.apply(lambda x: 1 if x == 'ddos' else 0)
-
-print("Vamos a tratar los NaN")
-# Variable de control para saber por que columna acabamos de analizar:
-
-
-# Dividimos los datos del DF:
-
-print("Voy a dividir los datos del dataset")
-data_X_train, data_X_test = train_test_split(data_X, test_size=0.2, random_state=42)
-data_Y_train, data_y_test = train_test_split(data_y, test_size=0.2, random_state=42)
-
-# Entrenar el modelo
-k = 5
-clf = KNeighborsClassifier(n_neighbors=k)
-
-print("Creación de subset.")
-
-subset_X_train = data_X_train.iloc[:, :15]
-subset_X_test = data_X_test.iloc[:, :15]
-
-print("Entrenar modelo.")
-clf.fit(subset_X_train, data_Y_train)
-
-print("Vamos a predecir.")
-data_y_pred = clf.predict(subset_X_test)
-
-accuracy = accuracy_score(data_y_test, data_y_pred)
-print(accuracy)
+print(f'El número de elementos diferentes en la primera columna ({primer_columna_nombre}) es: {num_elementos_diferentes}')
